4cbead5ee16dd44d054057cb8bd5aa98c6ff6013
Parker Moore <parkrmoore@gmail.com>
2012-05-17 01:49:16 -0400

    Photo no longer belongsTo anything.

--- app/Model/Photo.php
+++ app/Model/Photo.php
@@ -51,19 +51,4 @@ class Photo extends AppModel {
 	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
-
-/**
- * belongsTo associations
- *
- * @var array
- */
-	public $belongsTo = array(
-		'Item' => array(
-			'className' => 'Item',
-			'foreignKey' => 'item_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
-	);
 }
