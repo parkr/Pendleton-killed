38121c946c19e4cf7eb6950e2d2b3a80c63c0fa2
Parker Moore <parkrmoore@gmail.com>
2012-05-16 22:46:17 -0400

    Reconfigured AppController.

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,6 +32,9 @@ App::uses('Controller', 'Controller');
  * @link http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-	public $components = array('RequestHandler', 'Session');
-	public $helpers = 	 array('Form', 'Html', 'Js', 'Time');
+	
+	public $components = array(
+		'RequestHandler',
+		'Session'
+	);
 }
