d77f516e91202afa089a24bd201615809e2f077f
Parker Moore <parkrmoore@gmail.com>
2012-05-17 00:14:09 -0400

    Reworked urls for collections in the ajax function, and using years as parameter for ::view()

--- app/Controller/CollectionsController.php
+++ app/Controller/CollectionsController.php
@@ -23,12 +23,20 @@ class CollectionsController extends AppController {
  * @param string $id
  * @return void
  */
-	public function view($id = null) {
-		$this->Collection->id = $id;
-		if (!$this->Collection->exists()) {
-			throw new NotFoundException(__('Invalid collection'));
+	public function view($year = null) {
+		if($year == null){
+			if(isset($this->request->params['year'])){
+				$year = $this->request->params['year'];
+			} else {
+				throw new NotFoundException(__("Please specify the year of the collection."));
+			}
 		}
-		$this->set('collection', $this->Collection->read(null, $id));
+		$collection = $this->Collection->findByYear($year);
+		$this->Collection->id = $collection['Collection']['id'];
+		if (!$this->Collection->exists()) {
+			throw new NotFoundException(__("Collection for $year has not been added."));
+		}
+		$this->set('collection', $this->Collection->read(null, $collection['Collection']['id']));
 	}
 
 /**
@@ -112,7 +120,7 @@ class CollectionsController extends AppController {
 		);
 		$collections = $this->Collection->find('all', $params);
 		for($c=0; $c<count($collections); $c++){
-			$collections[$c]['Collection']['url'] = $Html->url(array('action' => 'view', $collections[$c]['Collection']['year']));
+			$collections[$c]['Collection']['url'] = $Html->url('/collections/'.urlencode($collections[$c]['Collection']['year']));
 		}
 		echo json_encode($collections);
 	}
